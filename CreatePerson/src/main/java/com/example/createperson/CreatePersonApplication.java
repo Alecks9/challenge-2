a3c212e2a83ef23e1b03c56289885172aa0277ca
[Alexander Perera] <[s3721805@student.rmit.edu.au]>
2022-10-10 23:41:39 +1100

    Initial commit

---
+++ CreatePerson/src/main/java/com/example/createperson/CreatePersonApplication.java
@@ -0,0 +1,13 @@
+package com.example.createperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatePersonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreatePersonApplication.class, args);
+    }
+
+}
